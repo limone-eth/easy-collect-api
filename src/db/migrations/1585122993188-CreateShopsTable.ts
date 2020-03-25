24f5fa6ff3614efdaca1fdc16dfcbc1a6751260a
simonestaffa <simone.staffa@tutored.it>
2020-03-25 14:42:13 +0100

    Created and popolated migrations file ready to be processed

--- src/db/migrations/1585122993188-CreateShopsTable.ts
+++ src/db/migrations/1585122993188-CreateShopsTable.ts
@@ -1,11 +1,82 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateShopsTable1585122993188 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.createTable(new Table({
+            name: "shops",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: "address",
+                    type: "varchar",
+                    isNullable: false
+                },
+                {
+                    name: "lat",
+                    type: "float",
+                    isNullable: false
+                },
+                {
+                    name: "lng",
+                    type: "float",
+                    isNullable: false
+                },
+                {
+                    name: "categories_id",
+                    type: "integer",
+                    isNullable: false
+                },
+                {
+                    name: "phone",
+                    type: "varchar",
+                    isNullable: true
+                },
+                {
+                    name: "telegram",
+                    type: "varchar",
+                    isNullable: true
+                },
+                {
+                    name: "facebook",
+                    type: "varchar",
+                    isNullable: true
+                },
+                {
+                    name: "is_deleted",
+                    type: "boolean",
+                    isNullable: false,
+                    default: false
+                },
+                {
+                    name: 'created_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+            ]
+        }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.dropTable("shops");
     }
 
 }
--- src/db/migrations/1585122998931-CreateCategoriesTable.ts
+++ src/db/migrations/1585122998931-CreateCategoriesTable.ts
@@ -1,11 +1,47 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateCategoriesTable1585122998931 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.createTable(new Table({
+            name: "categories",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: "is_deleted",
+                    type: "boolean",
+                    isNullable: false,
+                    default: false
+                },
+                {
+                    name: 'created_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+            ]
+        }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.dropTable("categories");
     }
 
 }
