7a3b9b9adf9777a08f2e2e2cd224dc9d2ad1d0e6
Halpph <cesaretti.1712254@studenti.uniroma1.it>
2020-03-25 23:11:39 +0100

    create request now adds link from shops to categories

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import * as bodyParser from 'body-parser';
 import {Shop} from "./db/models/Shop.model";
 import {connect} from "./db/db";
+import {ShopHasCategories} from "./db/models/Shop_Has_Categories";
+import {Column} from "typeorm";
 
 connect();
 
@@ -17,7 +19,7 @@ app.use(bodyParser.json({
 app.get('/', (req, res) => res.send('Hello World!'));
 
 // CREATE
-app.post('/shops', async (req,res) => {
+app.post('/shops', async (req, res) => {
     const shop = new Shop();
     shop.name = req.body.name;
     shop.address = req.body.address;
@@ -26,24 +28,39 @@ app.post('/shops', async (req,res) => {
     shop.phone = req.body.phone;
     shop.telegram = req.body.telegram;
     shop.facebook = req.body.facebook;
+    shop.categories = req.body.categories_ids;
     await shop.save();
+    if (shop.categories && shop.categories.length <= 3) {
+        for (const category of shop.categories) {
+            const shopHasCategories = new ShopHasCategories();
+            const shopLookup = await Shop.findOne({
+                where: {
+                    name: req.params.name
+                }
+            });
+            shopHasCategories.categories_id = category.id.toString();// not sure about string or Int, let's discuss later
+            shopHasCategories.shops_id = shopLookup.id.toString();   // not sure about string or Int, let's discuss later
+            shopHasCategories.natural_key = shopHasCategories.categories_id + "_" + shopHasCategories.shops_id;
+            await shopHasCategories.save();
+        }
+    }
     res.send(shop);
 });
 
 // READ
-app.get('/shops', async (req,res) => {
+app.get('/shops', async (req, res) => {
     const shops = await Shop.find();
     res.send(shops);
 });
 
 // READ SINGLE
-app.get('/shops/:id', async (req,res) => {
+app.get('/shops/:id', async (req, res) => {
     const shop = await Shop.findOne({
         where: {
             id: req.params.id
         }
     });
-    if (shop){
+    if (shop) {
         res.send(shop);
     } else {
         res.status(404).send({message: "Shop not found"})
@@ -51,32 +68,32 @@ app.get('/shops/:id', async (req,res) => {
 });
 
 // UPDATE
-app.put('/shops/:id', async (req,res) => {
+app.put('/shops/:id', async (req, res) => {
     const shop = await Shop.findOne({
         where: {
             id: req.params.id
         }
     });
-    if (shop){
+    if (shop) {
         if (req.body.name) {
             shop.name = req.body.name;
         }
-        if (req.body.address){
+        if (req.body.address) {
             shop.address = req.body.address;
         }
-        if (req.body.lat){
+        if (req.body.lat) {
             shop.lat = req.body.lat;
         }
-        if (req.body.lng){
+        if (req.body.lng) {
             shop.lng = req.body.lng;
         }
-        if (req.body.phone){
+        if (req.body.phone) {
             shop.phone = req.body.phone;
         }
-        if (req.body.telegram){
+        if (req.body.telegram) {
             shop.telegram = req.body.telegram;
         }
-        if (req.body.facebook){
+        if (req.body.facebook) {
             shop.facebook = req.body.facebook;
         }
         await shop.save();
@@ -87,13 +104,13 @@ app.put('/shops/:id', async (req,res) => {
 });
 
 // DELETE
-app.delete('/shops/:id', async (req,res) => {
+app.delete('/shops/:id', async (req, res) => {
     const shop = await Shop.findOne({
         where: {
             id: req.params.id
         }
     });
-    if (shop){
+    if (shop) {
         await shop.remove();
         res.send({message: 'Shop deleted'});
     } else {
