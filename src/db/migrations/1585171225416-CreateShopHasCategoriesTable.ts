71749e617221ed66a00442bd19460edf2513b44a
Halpph <cesaretti.1712254@studenti.uniroma1.it>
2020-03-25 22:25:38 +0100

    migration properly written

---
+++ src/db/migrations/1585171225416-CreateShopHasCategoriesTable.ts
@@ -0,0 +1,53 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateShopHasCategoriesTable1585171225416 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.createTable(new Table({
+            name: "shop_has_categories",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: "natural_key",
+                    type: "varchar",
+                    isNullable: false,
+                    isUnique: true
+                },
+                {
+                    name: "categories_id",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: "shops_id",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: 'created_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'datetime',
+                    isNullable: false,
+                    default: 'CURRENT_TIMESTAMP'
+                },
+            ]
+        }), true);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        return await queryRunner.dropTable("shop_has_categories");
+    }
+
+}
