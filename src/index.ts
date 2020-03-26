b5749f69736ccbb1f582b98c89da0b297bb781c7
simonestaffa <simonestaffa96@gmail.com>
2020-03-26 21:53:17 +0100

    HUGE refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {app} from './app'
 import {AddressInfo} from 'net'
 
 app.set('port', process.env.PORT || 5000);
-app.set('hostname', process.env.HOSTNAME || 'localhost');
+app.set('hostname', process.env.HOSTNAME || '0.0.0.0');
 
 const server = app.listen(app.get('port'), app.get('hostname'), () => {
     const {port, address} = server.address() as AddressInfo;
