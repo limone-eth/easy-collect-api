88b9754507afef2d25952bb4fa73b73a24041672
simonestaffa <simonestaffa96@gmail.com>
2020-03-26 21:58:26 +0100

    HUGE refactoring

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,13 @@ import {PathRoute} from "../routing-utilities/PathRoute";
 import {Route} from "../routing-utilities/Route";
 import {CategoriesRoutes} from "./categories";
 import {ShopsRoutes} from "./shops";
+import {CheckRoutes} from "./check";
 
 export class IndexRoute extends AbstractRoutes {
     initializePathRoutes(): PathRoute[] {
         return [
             new PathRoute('/categories', new CategoriesRoutes()),
+            new PathRoute('/check', new CheckRoutes()),
             new PathRoute('/shops', new ShopsRoutes()),
         ];
     }
